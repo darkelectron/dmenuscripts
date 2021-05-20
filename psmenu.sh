bfcebaa35f8ce8160e454e02c071465123cfc922
darkelectron <darkelectron@tutanota.com>
2021-05-20 23:13:31 +0000

    add psmenu script

---
+++ psmenu.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+# based on BugsWriter script
+
+if [ -n "$1" ]; then
+  prog="$@"
+else
+  prog="fzf"
+fi
+
+parse="$(ps -u $USER -o pid,%mem,%cpu,comm | sort -b -k2 -r | sed -n '1!p' | $prog | awk '{print $1}')"
+
+if [ -n "$parse" ]; then
+  kill -15 "$parse"
+fi
